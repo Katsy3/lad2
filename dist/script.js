ffcbf00793a75666b51b400ef83dbc29a721360c
Katsy3 <v.osolinskyi@gmail.com>
2025-10-05 13:37:30 +0300

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,1 @@
-function updateRating(workerId, inputId, ratingId) {
-  const input = document.getElementById(inputId);
-  let ratingValue = parseInt(input.value);
-  
-  // Перевірка, щоб значення було в межах 1-5
-  if (isNaN(ratingValue) || ratingValue < 1 || ratingValue > 5) {
-    alert("Оцінка має бути від 1 до 5!");
-    input.value = 1; // Повертаємо до мінімального значення
-    ratingValue = 1;
-  }
 
-  // Генерація зірок
-  let stars = "★".repeat(ratingValue) + "☆".repeat(5 - ratingValue);
-  document.getElementById(ratingId).textContent = `${stars} (${ratingValue}/5)`;
-}
